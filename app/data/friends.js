4b43db59f85dae5649d3dcda9a47697bdc2c95a9
Carden Pierre-Vil <pierrevil22@yahoo.com>
2019-05-11 12:17:24 -0400

    HW

---
+++ app/data/friends.js
@@ -0,0 +1,59 @@
+var friends = [ {
+    "name" : "Jack",
+    "photo" : "https://images.pexels.com/photos/748969/pexels-photo-748969.jpeg?cs=srgb&dl=friends-caves-funnypose-iphone-indian-india-indianmale-selfie-748969.jpg&fm=jpg",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Cherlie",
+    "photo" : "https://www.hellomagazine.com/imagenes/celebrities/2017121744850/scarlett-moffat-selfie-instagram/0-226-937/im%20a%20celeb%20t-t.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Kevin",
+    "photo" : "https://st2.depositphotos.com/2931363/6819/i/450/depositphotos_68197553-stock-photo-handsome-young-man-making-selfie.jpg",
+    "scores" : [5,5,5,5,5,5,5,5,5,5]
+}, {
+    "name" : "Amy",
+    "photo" : "https://i.dailymail.co.uk/i/pix/2015/10/26/14/2DCD6DBE00000578-3290026-image-a-107_1445868204107.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Josh",
+    "photo" : "https://www.pngkit.com/png/full/329-3294348_shawn-mendes-free-png-image-shawn-mendes-selfie.png",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Jenny",
+    "photo" : "https://www.beautycrew.com.au/media/27626/sam-forst-makeup-free-selfie-inset.jpg?width=675",
+    "scores" : [4,1,5,3,4,5,2,3,4,5]
+}, {
+    "name" : "Adam",
+    "photo" : "https://www.unbox.ph/wp-content/uploads/2017/11/IMG_0002.jpg",
+    "scores" : [3,4,2,4,4,1,1,2,3,4]
+}, {
+    "name" : "Jackie",
+    "photo" : "https://cdn.newsapi.com.au/image/v1/d6b149679a15458ae0f4e6020caf2274",
+    "scores" : [5,4,2,5,1,1,2,1,3,5]
+}, {
+    "name" : "Andre",
+    "photo" : "https://cdn7.dissolve.com/p/D1089_27_271/D1089_27_271_1200.jpg",
+    "scores" : [1,5,1,3,5,1,4,3,2,1]
+}, {
+    "name" : "Celine",
+    "photo" : "http://www.cheatsheet.com/wp-content/uploads/2017/01/emmy-rossum-makeup-free-selfie.jpg",
+    "scores" : [1,3,3,1,2,4,4,1,3,1]
+}, {
+    "name" : "Peter",
+    "photo" : "https://cdn8.dissolve.com/p/D1184_15_066/D1184_15_066_1200.jpg",
+    "scores" : [5,2,4,3,3,2,1,3,3,4]
+}, {
+    "name" : "Danielle",
+    "photo" : "https://ak3.picdn.net/shutterstock/videos/12177473/thumb/2.jpg",
+    "scores" : [1,1,1,1,1,1,1,1,1,1]
+}, {
+    "name" : "Chad",
+    "photo" : "https://cmkt-image-prd.global.ssl.fastly.net/0.1.0/ps/4130691/910/615/m1/fpnw/wm1/pyf6ratcx0kujih32pl4t07eagcdfsfjtivbca1oittipq2tfhybycest0k4ftym-.jpg?1521013769&s=408ba5e743522ce222878a4a37ce98b3",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}, {
+    "name" : "David",
+    "photo" : "https://remezcla.com/wp-content/uploads/2017/06/CgbgyR9UAAEPzk2.jpg?x80943",
+    "scores" : [2,3,5,5,3,5,2,1,3,2]
+}];
+
+module.exports = friends;
